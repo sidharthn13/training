ad46bf017ceabf12b16b4305128d2bfa01068d77
sidharth <sidharth.nair@neoito.com>
2023-11-13 13:19:33 +0530

    all combinations successfully generated

--- parantheseProblem.js
+++ parantheseProblem.js
@@ -1,26 +1,28 @@
-let output = []
-function main(num){
-    generate(num,num,'')
+let output = [];
+function generate_combination(num) {
+  output = [];
+  generate(num, num, "");
+  console.log(output);
 }
-function generate(open,closed,str){
-    if(open ==0 && closed == 0){
-        output.push(str)
+function generate(open, closed, str) {
+  if (open == 0 && closed == 0) {
+    //exit condition
+    output.push(str);
+  }
+  if (open == closed && open != 0) {
+    str += "(";
+
+    generate(open - 1, closed, str);
+  }
+
+  if (open < closed) {
+    str += ")";
+    generate(open, closed - 1, str);
+    if (open != 0) {
+      str = str.slice(0, str.length - 1);
+      str += "(";
+      generate(open - 1, closed, str);
     }
-    if(open == closed && open != 0){
-        str+= "("
-        console.log(str)
-        generate(open-1,closed,str)
-    }
-    if(open <closed && closed >0){
-        str += ')'
-        console.log(str)
-        generate(open,closed-1,str)
-    }
-    if(open > closed && open >0){
-        str+='('
-        console.log(str)
-        generate(open-1,closed,arr)
-    }
+  }
 }
-main(3)
-console.log(output)
+generate_combination(2);
---
+++ test.js
@@ -0,0 +1,5 @@
+let a = 'sidhu'
+a = a.slice(0,a.length-1)
+console.log(a)
+
+//returns sidhu
