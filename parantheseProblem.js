bcbd7fac4f075b99d0a6f379902dcc1bae3305cc
sidharth <sidharth.nair@neoito.com>
2023-11-13 13:50:18 +0530

    made optimisations an dimproved readability

--- parantheseProblem.js
+++ parantheseProblem.js
@@ -8,21 +8,21 @@ function generate(numberOfOpenBrackets, numberOfClosedBrackets, str) {
   if (numberOfOpenBrackets == 0 && numberOfClosedBrackets == 0) {
     //exit condition
     output.push(str);
+    return
   }
   if (numberOfOpenBrackets == numberOfClosedBrackets && numberOfOpenBrackets != 0) {
     str += "(";
 
     generate(numberOfOpenBrackets - 1, numberOfClosedBrackets, str);
   }
-
-  if (numberOfOpenBrackets < numberOfClosedBrackets) {
+  else{
     str += ")";
     generate(numberOfOpenBrackets, numberOfClosedBrackets - 1, str);
     if (numberOfOpenBrackets != 0) {
       str = str.slice(0, str.length - 1);
       str += "(";
       generate(numberOfOpenBrackets - 1, numberOfClosedBrackets, str);
-    }
-  }
+    }}
+  
 }
 generate_combination(5);
