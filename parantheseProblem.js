2307419c96d43e8c8ea196a5431d8b0bbbfb4e09
sidharth <sidharth.nair@neoito.com>
2023-11-13 11:52:33 +0530

    only first element in output successfully generated

--- parantheseProblem.js
+++ parantheseProblem.js
@@ -1,36 +1,26 @@
-let output =[]
-
+let output = []
 function main(num){
-    output = []
-    total = 2 * num
-    generate(num*2,num,num,[])
-} 
-
-function generate(total,numOpen,numClosed,arr){
-    if(arr.length ==  total){
-        output.push(arr)
-        return
+    generate(num,num,'')
+}
+function generate(open,closed,str){
+    if(open ==0 && closed == 0){
+        output.push(str)
     }
-    if(numOpen == numClosed && numOpen >0 && numClosed>0){
-        arr.push('(')
-        generate(total,numOpen-1,numClosed,arr)
-        generate(total,numOpen,numClosed-1,arr)
+    if(open == closed && open != 0){
+        str+= "("
+        console.log(str)
+        generate(open-1,closed,str)
     }
-    if(numOpen != numClosed){
-        if(numOpen == 0 && numClosed != 0){
-            arr.push(')')
-            generate(total,numOpen,numClosed - 1,arr)
-        }
-        else if(numOpen !=0 && numClosed == 0){
-            arr.push('(')
-            generate(total,numOpen-1,numClosed,arr)
-        }
-        else{
-        arr.push('(')
-        generate(total,numOpen-1,numClosed,arr)
-        arr.push(')')
-        generate(total,numOpen,numClosed-1,arr)}
+    if(open <closed && closed >0){
+        str += ')'
+        console.log(str)
+        generate(open,closed-1,str)
+    }
+    if(open > closed && open >0){
+        str+='('
+        console.log(str)
+        generate(open-1,closed,arr)
     }
 }
 main(3)
-output.forEach((el)=>console.log(el))
+console.log(output)
