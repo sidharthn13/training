782fc021e106f9d5dfff68995f5e8720b5bcd61d
sidharth <sidharth.nair@neoito.com>
2023-12-01 10:50:16 +0530

    used higher order function to make the code less verbose

--- multiplicativePersistence.js
+++ multiplicativePersistence.js
@@ -1,16 +1,31 @@
-function multiplicativePersistence(str) {
-  return reduceString(str, 0);
-}
-function reduceString(str, count) {
-  const arr = str.split("");
-  if (arr.length == 1) {
-    return [str, `number of steps: ${count}`];
-  }
-  let product = 1;
-  arr.forEach((element) => {
-    product *= element;
-  });
-  console.log(`step${count} --->  ${product}`);
-  return reduceString(`${product}`, count + 1);
-}
-console.log(multiplicativePersistence("277777788888899"));
+// function multiplicativePersistence(str) {
+//   return reduceString(str, 0);
+// }
+// function reduceString(str, count) {
+//   const arr = str.split("");
+//   if (arr.length == 1) {
+//     return [str, `number of steps: ${count}`];
+//   }
+//   let product = 1;
+//   arr.forEach((element) => {
+//     product *= element;
+//   });
+//   console.log(`step${count} --->  ${product}`);
+//   return reduceString(`${product}`, count + 1);
+// }
+// console.log(multiplicativePersistence("277777788888899"));
+
+
+
+//alternate method using Array.reduce:
+// function reduce(str, steps = 0) {
+//   if (str.length == 1) {
+//     return [str, steps];
+//   }
+//   const product = str.split("").reduce((accumulator, value) => {
+//     return accumulator * value;
+//   });
+//   console.log(`step${steps} ---> ${product}`);
+//   return reduce(`${product}`, steps + 1);
+// }
+// console.log(reduce("277777788888899"));
